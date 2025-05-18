37e7bdf33aefd53b93fd8c3f8350c3a6f97ee3db
Mohammed Firdous <124298708+mohammedfirdouss@users.noreply.github.com>
2025-05-18 11:03:07 +0000

    refactor the directory

--- app/backend/services/auth_services/app.py
+++ app/backend/services/auth_services/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
+import boto3 # For Cognito integration
 from flask_jwt_extended import (
     create_access_token, create_refresh_token, jwt_required, get_jwt_identity, JWTManager
 )
